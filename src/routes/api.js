b1be7cbb652b7577a037dd2a4b30004fbc41b35a
Tankerxyz <tankerxyz1@gmail.com>
2018-10-30 22:23:00 +0200

    add trip middleware

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,19 +6,18 @@ const { getAggregatedTrips } = require('../actions').trips;
 
 
 app.post('/trips', (req, res) => {
-    const query = {...config.defaultQueryOptions, ...req.body};
-    const uri = `${config.apiUri}&${getUriFromObj(query)}`;
+    const { tripQuery, tripUri } = req;
 
     doRequest({
-        uri,
+        uri: tripUri,
         json: true,
     })
     .then(async (response) => {
         const { trips } = response;
 
-        const directTrips = trips.filter((el) => el.departure_place.city_name == query.fn)
+        const directTrips = trips.filter((el) => el.departure_place.city_name == tripQuery.fn)
         const notDirectTrips = trips.filter(el => !directTrips.includes(el));
-        const aggregatedNotDirectTrips = await getAggregatedTrips(notDirectTrips, query.fn);
+        const aggregatedNotDirectTrips = await getAggregatedTrips(notDirectTrips, tripQuery.fn);
 
         const allTrips = directTrips.concat(aggregatedNotDirectTrips);
 
--- src/server.js
+++ src/server.js
@@ -6,14 +6,25 @@ const request = require("request-promise-native");
 const queryString = require('querystring');
 const moment = require("moment");
 
-const config = require('../config').server;
+const { blablaConfig, server: serverConfig } = require('../config');
 
 const routes = require('./routes');
 
+const { getTripQuery } = require('./utils');
+
 app.use(bodyParser.json());
+
+app.use('/api/trips', (req, res, next) => {
+    const query = {...blablaConfig.defaultQueryOptions, ...req.body};
+    const uri = getTripQuery(query);
+    req.tripUri = uri;
+    req.tripQuery = query;
+
+    next();
+});
 
 app.use(express.static('public'));
 
 app.use(routes);
 
-app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`))
+app.listen(serverConfig.port, () => console.log(`Example app listening on port ${serverConfig.port}!`))
